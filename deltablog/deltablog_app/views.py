fe7b56c1038d5921da7e16515ee3f525c369000f
Yiwei Gao <ygao56@emory.edu>
2015-02-02 20:03:25 -0500

    blog posts will now fully display when you click on the title

--- deltablog/deltablog_app/views.py
+++ deltablog/deltablog_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     posts = Post.objects.filter(published_date__isnull=False).order_by('published_date')
     return render(request, 'deltablog_app/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'deltablog_app/post_detail.html', {'post': post})
