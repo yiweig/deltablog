3cba23b17a2bf3ccdfd586b6e5c390f84cacfe95
Yiwei Gao <ygao56@emory.edu>
2015-02-02 20:27:09 -0500

    can now edit, publish, and remove posts!

--- deltablog/deltablog_app/views.py
+++ deltablog/deltablog_app/views.py
@@ -36,3 +36,17 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'deltablog_app/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'deltablog_app/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('deltablog_app.views.post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('deltablog_app.views.post_list')
